29153430215f35bc17f31e831fbba45a5b24f86f
Rotem Ovadia <rotemov@gmail.com>
2020-08-18 16:49:10 +0300

    LHC dataset download script for cluster.

---
+++ external_maf/datasets/data/lhc/lhc_download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1pxVwDzx7GXC4Ck0ZiHa6zukdpjnvy3m_' -O sig.npy
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Z8R5WXC2XWE7rEaI0glfCqSzrpGqE3_D' -O bg.npy
