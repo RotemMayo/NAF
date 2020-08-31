dbc389eeb06f4ece1afd5b5cec9bd4aa39840b2b
rotemov <rotemov@gmail.com>
2020-08-31 17:33:31 +0300

    Model test working.

--- test_model.py
+++ test_model.py
@@ -77,8 +77,11 @@ def main():
     sig = np.append(sig, np.ones((n_sig, 1)), axis=1)
 
     data = np.append(sig, bg, axis=0)
-
     sorted = data[(-data[:, 0]).argsort()]
+
+    for i in range(6):
+        n = 10**i
+        print("Number of signal in top events: [" + str(int(np.sum(sorted[:n, -1]))) + "/" + str(n) + "]")
 
     for i in range(sorted.shape[0]-1):
         if sorted[i, 0] < sorted[i + 1, 0]:
