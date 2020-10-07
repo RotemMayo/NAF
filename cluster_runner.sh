b612884817a87993e189a7c4a06320c41a233374
rotemov <rotemov@gmail.com>
2020-10-07 11:41:44 +0300

    Fixed cluster runner

--- cluster_runner.sh
+++ cluster_runner.sh
@@ -11,9 +11,11 @@
 
 INPUT_DIM=${1:-"7"}
 EXPERIMENT_NAME=${2:-""}
+SP=${3:-"0.1"}
+FLOW=${4:-"affine"}
 set --
 
 source /opt/anaconda3/bin/activate NAF
 cd /home/data/rotemov/Projects/NAF
-python maf_experiments.py --dataset 'lhc' --flowtype 'affine' --signal '0.1' --input_dim "$INPUT_DIM" --experiment_name "$EXPERIMENT_NAME"
+python maf_experiments.py --dataset "lhc" --flowtype "$FLOW" --signal "$SP" --input_dim "$INPUT_DIM" --experiment_name "$EXPERIMENT_NAME"
 echo "Done"
