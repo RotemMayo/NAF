dbc7830527ae2ddcaa7cc27c777306a179fd31d5
Rotem Ovadia <rotemov@gmail.com>
2020-10-27 20:31:17 +0200

    Fixed bug in runner

--- cluster_runner.sh
+++ cluster_runner.sh
@@ -26,6 +26,6 @@ source /opt/anaconda3/bin/activate NAF
 echo "env activated"
 python maf_experiments.py --dataset "lhc" --flowtype "$FLOW" --signal "$SP" --input_dim "$INPUT_DIM" --experiment_name "$EXPERIMENT_NAME"
 echo "model trained"
-python python test_model.py
+python test_model.py
 echo "test complete"
 echo "Done"
