1c8e68e8f577bb3da50940a0906151654cc7be1d
rotemov <rotemov@gmail.com>
2020-11-09 21:33:19 +0200

    added mjj-translation run to test runner

--- run_multiple_experiments.sh
+++ run_multiple_experiments.sh
@@ -3,10 +3,10 @@ R=$1
 SP=$2
 set --
 
-#/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_10000 "$SP"
-#/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_1000 "$SP"
-#/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_5000 "$SP"
-#/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_100 "$SP"
+/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_10000 "$SP"
+/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_1000 "$SP"
+/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_5000 "$SP"
+/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all_mjj-translation_100 "$SP"
 #/usr/bin/sbatch cluster_runner.sh 11 R"$R"_all "$SP"
 #/usr/bin/sbatch cluster_runner.sh 4 R"$R"_anode "$SP"
 #/usr/bin/sbatch cluster_runner.sh 4 R"$R"_salad "$SP"
