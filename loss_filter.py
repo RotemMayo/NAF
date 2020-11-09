51338c5ae819273e6bd6349ef4e561c41ae8266b
rotemov <rotemov@gmail.com>
2020-11-09 21:47:04 +0200

    fixed filter

--- loss_filter.py
+++ loss_filter.py
@@ -1,9 +1,11 @@
 from test_model import *
 
-INPUT_EXPERIMENT_NAME = "R0.4_all"
-SP = 0.1
-MODEL_NAME = "lhc_en{}_sp{}_e400_s1993_p0.0_h100_faffine_fl5_l1_dsdim16_dsl1_cudaFalse_best".format(INPUT_EXPERIMENT_NAME, SP)
+INPUT_ORIGINAL_EXPERIMENT_NAME = "R0.4_all"
 FILTER_NUMBER = 2
+PREV_FILTER_EXPERIMENT = "{}_filter_{}".format(INPUT_ORIGINAL_EXPERIMENT_NAME, FILTER_NUMBER-1)
+SP = 0.1
+MODEL_NAME = "lhc_en{}_sp{}_e400_s1993_p0.0_h100_faffine_fl5_l1_dsdim16_dsl1_cudaFalse_best".format(PREV_FILTER_EXPERIMENT, SP)
+
 
 REMOVE_SMALLEST = {
     1: 3*10**5,
@@ -21,13 +23,14 @@ OUTPUT_SIG_FILE_FORMAT = "{}lhc/sig_{}_filter_{}"
 
 def main():
     mdl = load_model(MODEL_NAME)
-    bg = np.nan_to_num(np.load('{}lhc/bg_{}_filter_{}.npy'.format(datasets.ROOT, INPUT_EXPERIMENT_NAME, FILTER_NUMBER-1)))
-    sig = np.nan_to_num(np.load('{}lhc/sig_{}_filter_{}.npy'.format(datasets.ROOT, INPUT_EXPERIMENT_NAME, FILTER_NUMBER-1)))
+    bg = np.nan_to_num(np.load('{}lhc/bg_{}.npy'.format(datasets.ROOT, PREV_FILTER_EXPERIMENT)))
+    sig = np.nan_to_num(np.load('{}lhc/sig_{}.npy'.format(datasets.ROOT, PREV_FILTER_EXPERIMENT)))
     bg_norm, sig_norm = normalize_data(mdl.args.signal_percent, bg, sig)
     print(bg.shape)
     print(sig.shape)
     n_bg = bg_norm.shape[0]
     bg_scores = get_scores(mdl, bg_norm)
+    print(bg_scores.shape)
     bg = np.append(bg_scores, bg, axis=1)
     bg = np.append(bg, np.zeros((n_bg, 1)), axis=1)
 
@@ -35,7 +38,8 @@ def main():
     sig_scores = get_scores(mdl, sig_norm)
     sig = np.append(sig_scores, sig, axis=1)
     sig = np.append(sig, np.ones((n_sig, 1)), axis=1)
-
+    print(bg.shape)
+    print(sig.shape)
     data = np.append(sig, bg, axis=0)
     sorted = data[(-data[:, 0]).argsort()]
 
@@ -45,8 +49,8 @@ def main():
     bg_new = np.ndarray([event[1:-1] for event in sorted if event[-1] == 0])
     print(sig_new.shape)
     print(bg_new.shape)
-    np.save(OUTPUT_BG_FILE_FORMAT.format(datasets.ROOT, INPUT_EXPERIMENT_NAME, FILTER_NUMBER), bg_new)
-    np.save(OUTPUT_SIG_FILE_FORMAT.format(datasets.ROOT, INPUT_EXPERIMENT_NAME, FILTER_NUMBER), sig_new)
+    np.save(OUTPUT_BG_FILE_FORMAT.format(datasets.ROOT, INPUT_ORIGINAL_EXPERIMENT_NAME, FILTER_NUMBER), bg_new)
+    np.save(OUTPUT_SIG_FILE_FORMAT.format(datasets.ROOT, INPUT_ORIGINAL_EXPERIMENT_NAME, FILTER_NUMBER), sig_new)
 
 
 if __name__ == "__main__":
